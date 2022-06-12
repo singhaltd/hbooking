22f8af38327b62916ea74e8323e545d51adfb8f4
singhaltd <85279370+singhaltd@users.noreply.github.com>
2022-06-12 22:36:10 +0700

    x

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
     fontFamily: {
       'sans': ['Lao_SomVang', 'Roboto'],
     },
+    fontSize:{
+      'base': '.7rem',
+      'md':'.8rem'
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
